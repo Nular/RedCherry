d12d3802c6e8b64d8a65cb64509df63c7a49ac32
Nular <AresNular@gmail.com>
2020-07-03 14:21:32 +0800

    初始模板以及基本结构

---
+++ pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+	
+	<view class="content">
+		<!-- <image class="logo" src="/static/logo.png"></image> -->
+		<view class="top">
+			<text class="step">
+				{{step}}步
+			</text>
+		</view>
+		<view class="center">
+			
+		</view>
+		<view class="bottom">
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				step: 2333
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>  
+	@import './index.css'; /*引入公共样式*/ 
+
+</style>
