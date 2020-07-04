28cffc8bf8a05c3b5af0c1bd6819cc950257737c
Nular <AresNular@gmail.com>
2020-07-04 11:12:50 +0800

    添加消息页面

--- pages/message/index.vue
+++ pages/message/index.vue
@@ -1,27 +1,38 @@
 <template>
-	
 	<view class="content">
-		<!-- <image class="logo" src="/static/logo.png"></image> -->
-		<view class="top">
-			<text class="step">
-				{{step}}步
-			</text>
+	
+	<view v-for="msg in messages" class="entry">
+		<view class="head">
+			<image :src="msg.head" mode="aspectFill" />
 		</view>
+		
 		<view class="center">
-			<view class="top"></view>
-			<view class="bottom"></view>
+			
+			<view></view>
+			<view></view>
 			
 		</view>
+		
+		<view class="time">{{ msg.time }}</view>
+	</view>
 		
 	</view>
 </template>
 
 <script>
+	
+	// message传值 例:
+	var massage = {
+		content: "今天天气真好",
+		time: new Date().toLocaleTimeString(),
+		sender: "天气问候者",
+		head: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3836099653,977060296&fm=26&gp=0.jpg"
+	}
 	export default {
 		data() {
 			return {
-				title: 'Hello',
-				step: 2333
+				step: 2333,
+				messages: [ massage , massage , massage , massage ]
 			}
 		},
 		onLoad() {
@@ -34,6 +45,23 @@
 </script>
 
 <style>  
-	@import './index.css'; /*引入公共样式*/ 
-
+	.content {
+		display: flex;
+		flex-direction: column;
+	}
+	.content.entry{
+		display: flex;
+		flex-direction: row;
+		
+		width: 100%;
+		height: 120rpx;
+	}
+	.content>.entry>.head{
+	
+	}
+	.content>.entry>.head>image{
+		height: 120rpx;
+		width: 120rpx;
+		
+	}
 </style>
