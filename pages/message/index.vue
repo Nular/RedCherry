20a0a92459b812b0975e299b511cf22dd1b2471a
Nular <AresNular@gmail.com>
2020-07-04 17:54:36 +0800

    重新提交消息界面

--- pages/message/index.vue
+++ pages/message/index.vue
@@ -8,8 +8,8 @@
 		
 		<view class="center">
 			
-			<view></view>
-			<view></view>
+			<view class="sender">{{msg.sender}}</view>
+			<view class="content">{{msg.content}}</view>
 			
 		</view>
 		
@@ -23,7 +23,7 @@
 	
 	// message传值 例:
 	var massage = {
-		content: "今天天气真好",
+		content: "今天天气真好吖hhhhhhh",
 		time: new Date().toLocaleTimeString(),
 		sender: "天气问候者",
 		head: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3836099653,977060296&fm=26&gp=0.jpg"
@@ -49,19 +49,45 @@
 		display: flex;
 		flex-direction: column;
 	}
-	.content.entry{
+	.content>.entry{
 		display: flex;
 		flex-direction: row;
 		
 		width: 100%;
 		height: 120rpx;
+		
 	}
 	.content>.entry>.head{
-	
+		height: 120rpx;
+		width: 120rpx;
 	}
 	.content>.entry>.head>image{
 		height: 120rpx;
 		width: 120rpx;
+	}
+	.content>.entry>.center{
+		height: 120rpx;
+		width: calc(100% - 120rpx - 180rpx);
+	}
+	.content>.entry>.center>.sender{
+		height: calc(50% - 20rpx);
+		margin-top: 20rpx;
+		font-size: 36rpx;
+		color: #007AFF;
+	}
+	.content>.entry>.center>.content{
+		height: calc(50% - 10rpx);
+		font-size: 24rpx;
+		margin-top: 10rpx;
+	}
+	.content>.entry>.time{
+		height: 120rpx;
+		width: 180rpx;
 		
+		font-size: 24rpx;
+		color: #333333;
+		
+		display: flex;
+		align-items: center;
 	}
 </style>
--- pages/warning/index.vue
+++ pages/warning/index.vue
@@ -1,27 +1,38 @@
 <template>
-	
 	<view class="content">
-		<!-- <image class="logo" src="/static/logo.png"></image> -->
-		<view class="top">
-			<text class="step">
-				{{step}}步
-			</text>
+	
+	<view v-for="msg in messages" class="entry">
+		<view class="head">
+			<image :src="msg.head" mode="aspectFill" />
 		</view>
+		
 		<view class="center">
-			<view class="top"></view>
-			<view class="bottom"></view>
+			
+			<view class="sender">{{msg.sender}}</view>
+			<view class="content">{{msg.content}}</view>
 			
 		</view>
+		
+		<view class="time">{{ msg.time }}</view>
+	</view>
 		
 	</view>
 </template>
 
 <script>
+	
+	// message传值 例:
+	var massage = {
+		content: "今天天气真好吖hhhhhhh",
+		time: new Date().toLocaleTimeString(),
+		sender: "天气问候者",
+		head: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3836099653,977060296&fm=26&gp=0.jpg"
+	}
 	export default {
 		data() {
 			return {
-				title: 'Hello',
-				step: 2333
+				step: 2333,
+				messages: [ massage , massage , massage , massage ]
 			}
 		},
 		onLoad() {
@@ -34,6 +45,49 @@
 </script>
 
 <style>  
-	@import './index.css'; /*引入公共样式*/ 
-
+	.content {
+		display: flex;
+		flex-direction: column;
+	}
+	.content>.entry{
+		display: flex;
+		flex-direction: row;
+		
+		width: 100%;
+		height: 120rpx;
+		
+	}
+	.content>.entry>.head{
+		height: 120rpx;
+		width: 120rpx;
+	}
+	.content>.entry>.head>image{
+		height: 120rpx;
+		width: 120rpx;
+	}
+	.content>.entry>.center{
+		height: 120rpx;
+		width: calc(100% - 120rpx - 180rpx);
+	}
+	.content>.entry>.center>.sender{
+		height: calc(50% - 20rpx);
+		margin-top: 20rpx;
+		font-size: 36rpx;
+		color: #007AFF;
+	}
+	.content>.entry>.center>.content{
+		height: calc(50% - 10rpx);
+		font-size: 24rpx;
+		margin-top: 10rpx;
+	}
+	.content>.entry>.time{
+		height: 120rpx;
+		width: 180rpx;
+		
+		font-size: 24rpx;
+		color: #333333;
+		
+		display: flex;
+		align-items: center;
+	}
 </style>
